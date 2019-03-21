44a15d6de3df1f73d5b7ada504a7c4aaa12ca23d
vincenteof <vincenteof@qq.com>
2019-03-21 21:56:14 +0800

    createStore finished

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ interface Action {
   [propName: string]: any
 }
 
-type Reducer = (state: any, action: Action) => any
+// todo: does this definition make the invariant that a reducer may never transfrom state to undefined???
+type NotUndefined = string | number | boolean | symbol | object
+type Reducer = (state: any, action: Action) => NotUndefined
 
-type StoreListener = (getState: () => any) => void
+type StoreListener = () => void
 type StoreUnsubscriber = () => void
 
 interface IStore {
   getState(): any
-  dispatch(action: Action): void
+  dispatch(action: Action): Action
   subscrible(listener: StoreListener): StoreUnsubscriber
 }
 
@@ -18,32 +20,64 @@ class Store implements IStore {
   private state: any
   private reducer: Reducer
   private listeners: StoreListener[]
+  private dispatching: boolean
 
   constructor(reducer: Reducer, preloadState: any) {
     this.reducer = reducer
     this.state = preloadState
     this.listeners = []
+    this.dispatching = false
   }
 
   getState() {
-    // todo: modification to the result should not interfere with inner state
+    if (this.dispatching) {
+      throw new Error(
+        'You should not call store.getState() in reducer, ' +
+          'use the provided state parameter instead.'
+      )
+    }
     return this.state
   }
 
-  dispatch(action: Action): void {
-    // todo: deal with multi-dispatching???
-    const nextState = this.reducer(this.state, action)
-    this.state = nextState
-    const giveNewState = () => nextState
-    this.listeners.forEach(listener => listener(giveNewState))
+  dispatch(action: Action): Action {
+    if (this.dispatching) {
+      throw new Error('Reducers may not dispatch actions.')
+    }
+
+    this.dispatching = true
+    try {
+      const nextState = this.reducer(this.state, action)
+      // so we cannot modify inner state by modifying the result of `getState`
+      if (nextState instanceof Object) {
+        Object.freeze(nextState)
+      }
+      this.state = nextState
+    } finally {
+      this.dispatching = false
+    }
+
+    this.listeners.forEach(listener => listener())
+    return action
   }
 
   subscrible(listener: StoreListener): StoreUnsubscriber {
+    if (this.dispatching) {
+      throw new Error('You should not call store.subscrible() in reducer')
+    }
+
+    // this flag is used to solve the problem that same listener subscribles twice
+    // while the previous used unsubscriber still works
+    let isSubscribled = true
     this.listeners.push(listener)
+
     return () => {
+      if (!isSubscribled) {
+        return
+      }
       const pos = this.listeners.indexOf(listener)
       if (pos >= 0) {
         this.listeners.splice(pos, 1)
+        isSubscribled = false
       }
     }
   }
--- test/store.test.ts
+++ test/store.test.ts
@@ -1,16 +1,29 @@
 import { createStore, Action } from '../src/index'
 
 describe('store creation and basic usage', () => {
-  it('creates a store with simple state', () => {
-    const reducer = (state: any = '', action: Action) => {
-      switch (action.type) {
-        case 'INIT':
-          return 'inited'
-        default:
-          return state
-      }
+  const reducer = (state: any = '', action: Action) => {
+    switch (action.type) {
+      case 'FIRST':
+        return 'First'
+      case 'SECOND':
+        return {
+          value: 'Second'
+        }
+      default:
+        return state
     }
+  }
+
+  it('creates a store with simple state', () => {
     const store = createStore(reducer, '')
     expect(store.getState()).toBe('')
+  })
+
+  it('dispatches some actions', () => {
+    const store = createStore(reducer, '')
+    store.dispatch({ type: 'FIRST' })
+    expect(store.getState()).toBe('First')
+    store.dispatch({ type: 'SECOND' })
+    expect(store.getState().value).toBe('Second')
   })
 })
